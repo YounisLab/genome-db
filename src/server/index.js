8a51fc7f539fad40d5690d6de4148f6699d3392a
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-02-09 16:25:27 +0300

    Connect backend to postgres at startup

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { URL } = require('url')
+const adapter = require('./adapter')
 const app = express()
 const port = 8080
+const dbURL = (new URL('postgres://genomedb:genomedb@postgres:5432/genomedb'))
+
+adapter.connect(dbURL)
+  .then(function () {
+    app.listen(port, () => console.log('GenomeDB serving on', port))
+  })
+  .catch(function (err) {
+    console.error(err)
+    process.exit(1)
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello world\n')
