638ade02f87f7c8c41257eb35e892db34fdea464
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-02-11 22:33:30 +0300

    Add styles.js for sharing style

---
+++ src/client/styles.js
@@ -0,0 +1,21 @@
+// Global styles
+
+const contentStyle = {
+  padding: '20px'
+}
+
+const contentDivStyle = {
+  background: '#fff',
+  padding: 24,
+  minHeight: 280
+}
+
+const rowStyle = {
+  padding: '5px 0'
+}
+
+module.exports = {
+  rowStyle: rowStyle,
+  contentStyle: contentStyle,
+  contentDivStyle: contentDivStyle
+}
