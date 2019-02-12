cfe8f90a7515d1c7e32c5b148f2a6f1a5b5fad5c
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-02-12 13:57:50 +0300

    Remove node-pg logger

--- src/server/adapter.js
+++ src/server/adapter.js
@@ -2,17 +2,12 @@ const { Pool } = require('pg')
 
 var pool
 
-const nodepgLogger = function (msg) {
-  console.log('node-pg: %s', msg)
-}
-
 module.exports = {
   connect: function (urlObject) {
     var host = urlObject.host
     console.log('Connecting to postgres at', host)
     pool = new Pool({
-      connectionString: urlObject.href,
-      log: nodepgLogger
+      connectionString: urlObject.href
     })
 
     return pool.query('SELECT NOW() as now')
