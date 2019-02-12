17de7a944255b4ee3ad7063ce9faa55a740e799c
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-02-12 15:26:04 +0300

    Add log2fpkm columns to seed data

--- src/server/seed/init.sql
+++ src/server/seed/init.sql
@@ -1,13 +1,15 @@
 CREATE TABLE MCF10A
 (
     gene VARCHAR,
-    fpkm FLOAT
+    fpkm FLOAT,
+    log2 FLOAT
 );
 
 CREATE TABLE MCF7
 (
     gene VARCHAR,
-    fpkm FLOAT
+    fpkm FLOAT,
+    log2 FLOAT
 );
 
 CREATE TABLE MCF10A_vs_MCF7
@@ -17,6 +19,6 @@ CREATE TABLE MCF10A_vs_MCF7
     log2_foldchange FLOAT
 );
 
-COPY MCF10A FROM PROGRAM 'cut -f 1,2 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
-COPY MCF7   FROM PROGRAM 'cut -f 1,3 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF10A FROM PROGRAM 'cut -f 1,2,6 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF7   FROM PROGRAM 'cut -f 1,3,7 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
 COPY MCF10A_vs_MCF7 FROM PROGRAM 'cut -f 1,4,5 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
