6fc96185f12f5b69b201d058fc86744858d9660a
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-02-13 11:41:58 +0300

    Remove app.js

--- src/server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello world\n')
-})
-
-app.listen(port, () => console.log('GenomeDB serving on ', port))
