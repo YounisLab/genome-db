a76360811cdb887737f13b4401c3c45d5c04f41a
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-02-28 18:44:02 +0300

    Use separated data files for postgres init

--- src/server/seed/init.sql
+++ src/server/seed/init.sql
@@ -19,6 +19,6 @@ CREATE TABLE MCF10A_vs_MCF7
     pvalue FLOAT
 );
 
-COPY MCF10A FROM PROGRAM 'cut -f 1,2,6 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
-COPY MCF7   FROM PROGRAM 'cut -f 1,3,7 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
-COPY MCF10A_vs_MCF7 FROM PROGRAM 'cut -f 1,4,5 /docker-entrypoint-initdb.d/data/MCF10A_MCF7_gene_exp.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF10A FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF7.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF7   FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF10A_vs_MCF7 FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A_vs_MCF7.csv' DELIMITER E'\t' CSV HEADER;
