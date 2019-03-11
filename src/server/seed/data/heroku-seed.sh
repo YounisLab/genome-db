f6d5221446c0928cf1b5d6f018c3eeaaf8a69be8
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-03-11 19:53:20 +0300

    Update heroku script to reset DB before seeding

--- src/server/seed/data/heroku-seed.sh
+++ src/server/seed/data/heroku-seed.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+psql `heroku config:get DATABASE_URL` -c "DROP TABLE MCF10A;"
+psql `heroku config:get DATABASE_URL` -c "DROP TABLE MCF7;"
+psql `heroku config:get DATABASE_URL` -c "DROP TABLE MCF10A_vs_MCF7;"
+
+psql `heroku config:get DATABASE_URL` -c \
+"CREATE TABLE MCF10A(gene VARCHAR,fpkm FLOAT,log2 FLOAT);"
+
+psql `heroku config:get DATABASE_URL` -c \
+"CREATE TABLE MCF7(gene VARCHAR,fpkm FLOAT,log2 FLOAT);"
+
+psql `heroku config:get DATABASE_URL` -c \
+"CREATE TABLE MCF10A_vs_MCF7(gene VARCHAR,log2_foldchange FLOAT,pvalue FLOAT);"
+
 cat MCF10A.csv | \
 psql `heroku config:get DATABASE_URL` -c "COPY MCF10A FROM STDIN DELIMITER E'\t' CSV HEADER;"
 
