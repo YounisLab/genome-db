d8e29e78462713a028b7e93e9e3b4f9bf097c393
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-03-11 19:20:40 +0300

    Add RBP_pvalue table to postgres init script

--- src/server/seed/init.sql
+++ src/server/seed/init.sql
@@ -19,6 +19,13 @@ CREATE TABLE MCF10A_vs_MCF7
     pvalue FLOAT
 );
 
+CREATE TABLE RBP_pvalues
+(
+    gene VARCHAR,
+    pvalue JSONB
+);
+
 COPY MCF10A FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF7.csv' DELIMITER E'\t' CSV HEADER;
 COPY MCF7   FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A.csv' DELIMITER E'\t' CSV HEADER;
 COPY MCF10A_vs_MCF7 FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A_vs_MCF7.csv' DELIMITER E'\t' CSV HEADER;
+COPY RBP_pvalues FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/RBP_pvalue.csv' DELIMITER E'\t' CSV HEADER QUOTE '"' ESCAPE '\';
