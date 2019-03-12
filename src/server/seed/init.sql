dcee43dde716c4fdb1b30c4a1acb77cd9b1bab0e
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-03-12 23:02:21 +0300

    Swap sample names in postgres init

--- src/server/seed/init.sql
+++ src/server/seed/init.sql
@@ -25,7 +25,7 @@ CREATE TABLE RBP_rvalues
     rvalue JSONB
 );
 
-COPY MCF10A FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF7.csv' DELIMITER E'\t' CSV HEADER;
-COPY MCF7   FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF10A FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A.csv' DELIMITER E'\t' CSV HEADER;
+COPY MCF7   FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF7.csv' DELIMITER E'\t' CSV HEADER;
 COPY MCF10A_vs_MCF7 FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/MCF10A_vs_MCF7.csv' DELIMITER E'\t' CSV HEADER;
 COPY RBP_rvalues FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/RBP_rvalues.csv' DELIMITER E'\t' CSV HEADER QUOTE '"' ESCAPE '\';
