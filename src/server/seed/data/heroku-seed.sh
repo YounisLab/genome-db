481dbfc1cbb896b3f2fe3e9f8c305e190ccbccec
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-04-20 18:10:03 +0300

    Modify script to seed RBP_RVALUES to Heroku

--- src/server/seed/data/heroku-seed.sh
+++ src/server/seed/data/heroku-seed.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+# CREATE TABLES
+
 psql `heroku config:get DATABASE_URL` -c "DROP TABLE MCF10A_vs_MCF7;"
 
 psql `heroku config:get DATABASE_URL` -c \
 "CREATE TABLE MCF10A_vs_MCF7 (gene VARCHAR, mcf10a_fpkm FLOAT, mcf10a_log2 FLOAT, mcf7_fpkm FLOAT, mcf7_log2 FLOAT, log2_foldchange FLOAT, pvalue FLOAT);"
 
+psql `heroku config:get DATABASE_URL` -c "DROP TABLE RBP_RVALUES;"
+
+psql `heroku config:get DATABASE_URL` -c \
+"CREATE TABLE RBP_RVALUES ( gene VARCHAR, rvalue JSONB );"
+
+# UPLOAD DATA
+
 cat mcf10a_vs_mcf7.csv | \
 psql `heroku config:get DATABASE_URL` -c "COPY MCF10A_vs_MCF7 FROM STDIN DELIMITER E'\t' CSV HEADER;"
+
+cat RBP_rvalues.csv | \
+psql `heroku config:get DATABASE_URL` -c "COPY RBP_RVALUES FROM STDIN DELIMITER E'\t' CSV HEADER QUOTE '\"' ESCAPE '\';"
