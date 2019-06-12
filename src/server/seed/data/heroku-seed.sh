b105e1496f265cd5a93a7b5877b11f45f36682cb
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-06-13 01:39:08 +0300

    Add MCF intron average psi data (#18)
    * Add mcf_avg_psi.csv to git-lfs
    * Update init.sql script with mcf_avg_psi
    * Update heroku-seed script with mcf_avg_psi

--- src/server/seed/data/heroku-seed.sh
+++ src/server/seed/data/heroku-seed.sh
@@ -2,17 +2,17 @@
 
 # CREATE TABLES
 
-psql `heroku config:get DATABASE_URL` -c "DROP TABLE MCF10A_vs_MCF7;"
 psql `heroku config:get DATABASE_URL` -c \
 "CREATE TABLE MCF10A_vs_MCF7 (gene VARCHAR, mcf10a_fpkm FLOAT, mcf10a_log2 FLOAT, mcf7_fpkm FLOAT, mcf7_log2 FLOAT, log2_foldchange FLOAT, pvalue FLOAT);"
 
-psql `heroku config:get DATABASE_URL` -c "DROP TABLE RBP_RVALUES;"
 psql `heroku config:get DATABASE_URL` -c \
 "CREATE TABLE RBP_RVALUES ( gene VARCHAR, rvalue JSONB );"
 
-psql `heroku config:get DATABASE_URL` -c "DROP TABLE MCF_INTRON_PSI;"
 psql `heroku config:get DATABASE_URL` -c \
 "CREATE TABLE MCF_INTRON_PSI ( gene VARCHAR, intron_number INT CHECK (intron_number > 0), mcf10a_psi FLOAT, mcf7_psi FLOAT );"
+
+psql `heroku config:get DATABASE_URL` -c \
+"CREATE TABLE MCF_AVG_PSI ( gene VARCHAR, mcf10a_avg_psi FLOAT, mcf7_avg_psi FLOAT );"
 
 # UPLOAD DATA
 
@@ -24,3 +24,6 @@ psql `heroku config:get DATABASE_URL` -c "COPY RBP_RVALUES FROM STDIN DELIMITER 
 
 cat mcf_intron_psi.csv | \
 psql `heroku config:get DATABASE_URL` -c "COPY MCF_INTRON_PSI FROM STDIN DELIMITER E'\t' CSV HEADER;"
+
+cat mcf_avg_psi.csv | \
+psql `heroku config:get DATABASE_URL` -c "COPY MCF_AVG_PSI FROM STDIN DELIMITER E',' CSV HEADER;"
--- src/server/seed/init.sql
+++ src/server/seed/init.sql
@@ -23,6 +23,14 @@ CREATE TABLE mcf_intron_psi
     mcf7_psi FLOAT
 );
 
+CREATE TABLE mcf_avg_psi
+(
+    gene VARCHAR,
+    mcf10a_avg_psi FLOAT,
+    mcf7_avg_psi FLOAT
+);
+
 COPY MCF10A_vs_MCF7 FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/mcf10a_vs_mcf7.csv' DELIMITER E'\t' CSV HEADER;
 COPY RBP_rvalues FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/RBP_rvalues.csv' DELIMITER E'\t' CSV HEADER QUOTE '"' ESCAPE '\';
 COPY mcf_intron_psi FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/mcf_intron_psi.csv' DELIMITER E'\t' CSV HEADER;
+COPY mcf_avg_psi FROM PROGRAM 'cat /docker-entrypoint-initdb.d/data/mcf_avg_psi.csv' DELIMITER E',' CSV HEADER;
