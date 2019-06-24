2bcc6e044d64d24db250a2ff540d906c04582aea
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2019-06-24 14:26:17 +0300

    Set HeatMap range using percentiles (#23)
    Initially set it to use min and max of 25% quartiles.

--- src/client/HeatMapChart.js
+++ src/client/HeatMapChart.js
@@ -6,6 +6,27 @@ const _ = require('lodash')
 
 const samples = ['mcf10a', 'mcf7']
 
+// data should look like [[0,0, 1.2], [0,1, 2.3] ...]
+// quartile should be a float: eg 0.25 implies
+// drop bottom 25% and top 25% of all values.
+// Returns min and max after dropping
+function getQuartiles (data, quartile) {
+  var sortedData = _.orderBy(data, function (datum) {
+    return datum[2]
+  })
+
+  var numToDrop = Math.floor(sortedData.length * quartile)
+  var dropped = _.dropRight(
+    _.drop(sortedData, numToDrop),
+    numToDrop
+  )
+
+  return {
+    min: _.first(dropped)[2],
+    max: _.last(dropped)[2]
+  }
+}
+
 function createHeatMapSeries (data, samples) {
   var series = []
   _.each(samples, function (sample, xIndex) {
@@ -22,6 +43,8 @@ function createHeatMapSeries (data, samples) {
   return series
 }
 
+const QUARTILE_FLOAT = 0.25
+
 class HeatMapChart extends React.Component {
   constructor (props) {
     super(props)
@@ -29,15 +52,23 @@ class HeatMapChart extends React.Component {
     this.state = {
       yAxisCategories: [],
       xAxisCategories: samples,
-      data: []
+      data: [],
+      min: 0,
+      max: 0
     }
   }
   componentDidUpdate (prevProps) {
     if (this.props.data && this.props.data.length > 0 &&
         this.props.data !== prevProps.data) {
       var data = createHeatMapSeries(this.props.data, this.state.xAxisCategories)
+      var range = getQuartiles(data, QUARTILE_FLOAT)
       var yAxisCategories = _.map(this.props.data, (d) => d.gene)
-      this.setState({ data: data, yAxisCategories: yAxisCategories })
+      this.setState({
+        data: data,
+        yAxisCategories: yAxisCategories,
+        min: range.min,
+        max: range.max
+      })
     }
   }
   render () {
@@ -74,8 +105,8 @@ class HeatMapChart extends React.Component {
               title: 'Genes'
             },
             colorAxis: {
-              min: -10,
-              max: 10,
+              min: this.state.min,
+              max: this.state.max,
               stops: [
                 [0, '#0000FF'],
                 [0.5, '#FFFFFF'],
