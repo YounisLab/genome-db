6a4ddd39da93a926cc4f3a286749b7db9dc37a12
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2020-03-06 12:31:03 +0300

    Add HeatMapChart component

---
+++ src/client/components/charts/HeatMapChart.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+require('highcharts/modules/heatmap')(Highcharts)
+
+/*
+  props = {
+    xAxisCategories: categories for labelling x-axis
+    yAxisCategories: categories for labelling y-axis
+    yAxisMin:
+    yAxisMax:
+    tooltip_formatter:
+    series: data to be drawn (HighCharts series format)
+  }
+*/
+class HeatMapChart extends React.Component {
+  render () {
+    return (
+      <div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={{
+            chart: {
+              type: 'heatmap',
+              marginTop: 40,
+              marginBottom: 80,
+              plotBorderWidth: 1,
+              zoomType: 'y',
+              height: this.props.series.length < 100 ? 500 : 1500
+            },
+            series: [{
+              turboThreshold: 0,
+              borderWidth: 1,
+              data: this.props.series,
+              dataLabels: {
+                enabled: false,
+                color: '#000000'
+              }
+            }],
+            title: {
+              text: null
+            },
+            xAxis: {
+              categories: this.props.xAxisCategories,
+              labels: {
+                style: {
+                  fontSize: '15px'
+                }
+              }
+            },
+            yAxis: {
+              categories: this.props.yAxisCategories,
+              title: 'Genes',
+              labels: {
+                style: {
+                  fontSize: '15px'
+                }
+              }
+            },
+            colorAxis: {
+              min: this.props.yAxisMin,
+              max: this.props.yAxisMax,
+              stops: [
+                [0, '#0000FF'],
+                [0.5, '#FFFFFF'],
+                [0.9, '#FF0000']
+              ],
+              labels: {
+                style: {
+                  fontSize: '12px'
+                }
+              }
+            },
+            legend: {
+              align: 'right',
+              layout: 'vertical',
+              margin: 0,
+              verticalAlign: 'top',
+              y: 25,
+              symbolHeight: 280
+            },
+            tooltip: {
+              formatter: this.props.tooltip_formatter
+            }
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export { HeatMapChart }
--- src/client/components/charts/index.js
+++ src/client/components/charts/index.js
@@ -1,1 +1,2 @@
 export * from './BellCurveChart'
+export * from './HeatMapChart'
