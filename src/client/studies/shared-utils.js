2949d730927eea5e24ee889c18d8cc6bb9468314
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2020-03-06 12:30:40 +0300

    Add getQuartiles to shared-utils

--- src/client/studies/shared-utils.js
+++ src/client/studies/shared-utils.js
@@ -48,3 +48,24 @@ export function createHeatMapSeries (data, samples) {
 
   return series
 }
+
+// data should look like [[0,0, 1.2], [0,1, 2.3] ...]
+// quartile should be a float: eg 0.25 implies
+// drop bottom 25% and top 25% of all values.
+// Returns min and max after dropping
+export function getQuartiles (data, quartile) {
+  var sortedData = _.orderBy(data, function (datum) {
+    return datum[2]
+  })
+
+  var numToDrop = Math.floor(sortedData.length * quartile)
+  var dropped = _.dropRight(
+    _.drop(sortedData, numToDrop),
+    numToDrop
+  )
+
+  return {
+    min: _.first(dropped)[2],
+    max: _.last(dropped)[2]
+  }
+}
